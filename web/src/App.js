a83772a86d2f15caa55dd66fda511e5bae22c7ba
Marcos Willian <markusuuuu@gmail.com>
2020-05-03 17:42:25 -0300

    global styles

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <div>
       <Main />
+      <GlobalStyle />
     </div>
   );
 }
