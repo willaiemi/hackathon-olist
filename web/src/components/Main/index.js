eceac8a285ff510b485c4114ec83cef20003a454
Willian Duarte <willianm.cduarte@gmail.com>
2020-05-03 17:02:51 -0300

    Button export

--- web/src/components/Main/index.js
+++ web/src/components/Main/index.js
@@ -14,9 +14,38 @@ function Main() {
         bottom: '0px',
         width: '100%',
     // /\ remove /\
+        display: 'flex',
       }}
     >
       <Sidebar></Sidebar>
+      <div
+        style={{
+          height: '100%',
+          width: '980px',
+          background: 'rgba(0, 255, 0, 0.1)',
+          padding: '34px 34px 0 0',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <button
+            style={{
+              height: '30px',
+              width: '120px',
+              border: '1px solid #97a3b5',
+              color: '#97a3b5',
+              background: 'white',
+              borderRadius: '24px',
+            }}
+          >
+            Exportar
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
