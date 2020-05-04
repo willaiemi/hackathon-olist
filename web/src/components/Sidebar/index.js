42a71c812a682d219f1994faa237352db4fdec46
Willian Duarte <willianm.cduarte@gmail.com>
2020-05-04 13:02:06 -0300

    Extracting Sidebar styles

--- web/src/components/Sidebar/index.js
+++ web/src/components/Sidebar/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
+import {
+  SidebarContainer,
+  GreyContainer,
+  Navbar,
+  NavbarItem,
+} from './style';
 
 const propTypes = {
   
@@ -7,107 +13,25 @@ const propTypes = {
 
 function Sidebar({}) {
   return (
-    <div
-      style={{
-        width: '300px',
-        height: '100%',
-        background: 'white',
-      }}
-    >
-      <div
-        style={{
-          width: '290px',
-          background: '#f4f4f4',
-          height: '100%',
-          borderRadius: '34px 0px 0px 34px',
-          paddingBottom: '34px',
-          overflow: 'visible scroll',
-        }}
-      >
-        <h2
-          style={{
-            color: '#2b2b2b',
-            fontSize: '16px',
-            padding: '34px 0px 17px 34px',
-          }}
-        >
-          Últimas perguntas
-        </h2>
+    <SidebarContainer>
+      <GreyContainer>
+        <h2>Últimas perguntas</h2>
         
-        <div
-          style={{
-            height: '254px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            color: '#0045ff',
-            lineHeight: '30px',
-          }}
-        >
-          <p
-            style={{
-              paddingLeft: '34px',
-              fontSize: '16px',
-              width: '300px',
-              background: '#0045ff',
-              color: 'white',
-              borderRadius: '0px 34px 34px 0px',
-            }}
-          >
+        <Navbar>
+          <NavbarItem active>
             Todas as perguntas
-          </p>
-          <p
-            style={{
-              paddingLeft: '34px',
-              fontSize: '16px'
-            }}
-          >Entrega</p>
-          <p
-            style={{
-              paddingLeft: '34px',
-              fontSize: '16px'
-            }}
-          >Funcionalidade</p>
-          <p
-            style={{
-              paddingLeft: '34px',
-              fontSize: '16px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-start',
-            }}
-          >Pagamento
-            <span
-              style={{
-                height: '24px',
-                padding: '0px 7px',
-                background: '#ff1515',
-                color: 'white',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginLeft: '10px',
-                borderRadius: '50px',
-                fontSize: '12px',
-                fontWeight: 'bold',
-              }}
-            >100</span>
-          </p>
-          <p
-            style={{
-              paddingLeft: '34px',
-              fontSize: '16px'
-            }}
-          >Cancelamento</p>
-          <p
-            style={{
-              paddingLeft: '34px',
-              fontSize: '16px'
-            }}
-          >Descrição</p>
-        </div>
-      </div>
-    </div>
+          </NavbarItem>
+          <NavbarItem>Entrega</NavbarItem>
+          <NavbarItem>Funcionalidade</NavbarItem>
+          <NavbarItem>
+            Pagamento
+            <span>100</span>
+          </NavbarItem>
+          <NavbarItem>Cancelamento</NavbarItem>
+          <NavbarItem>Descrição</NavbarItem>
+        </Navbar>
+      </GreyContainer>
+    </SidebarContainer>
   )
 }
 
--- web/src/components/Sidebar/style.js
+++ web/src/components/Sidebar/style.js
@@ -1,6 +1,63 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  width: 400px;
+export const SidebarContainer = styled.div`
+  width: 300px;
   height: 100%;
+  background: white;
+`;
+
+export const GreyContainer = styled.div`
+  width: 290px;
+  background: #f4f4f4;
+  height: 100%;
+  border-radius: 34px 0px 0px 34px;
+  padding-bottom: 34px;
+  overflow: visible scroll;
+
+  h2 {
+    color: #2b2b2b;
+    font-size: 16px;
+    padding: 34px 0px 17px 34px;
+  }
+`;
+
+export const Navbar = styled.ul`
+  height: 254px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: #0045ff;
+  line-height: 30px;
+`;
+
+export const NavbarItem = styled.li`
+  padding-left: 34px;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  cursor: pointer;
+
+  ${props => props.active && `
+    padding-left: 34px;
+    font-size: 16px;
+    width: 300px;
+    background: #0045ff;
+    color: white;
+    borderRadius: 0px 34px 34px 0px;
+  `}
+
+  span {
+    height: 24px;
+    padding: 0px 7px;
+    background: #ff1515;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 10px;
+    border-radius: 50px;
+    font-size: 12px;
+    font-weight: bold;
+  }
 `;
